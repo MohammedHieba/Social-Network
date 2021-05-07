e1093aff475b3737b8e2f538d15210cd322e922e
abdelrahmany0 <abdelrhmanatta01@gmail.com>
2021-05-07 03:12:45 +0200

    enhancing friendship

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("friends", views.index, name="accounts_index"),
-    path("friendship/<receiver_id>", views.friendship, name="accounts_friendship"),
+    path("friends", views.index, name="get_friends"),
+    path("add_friend/<receiver_id>", views.friend_request, name="send_friend_request"),
+    path("friend_requests", views.friend_requests, name="friend_requests"),
+    path("accept_friend/<request_id>", views.accept_friend, name="accept_friend"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.contrib import messages
 from django.http import HttpResponseRedirect
 
-from accounts.models import User, Friendship
+from .models import User, Friendship
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import UserCreationForm
@@ -12,16 +12,38 @@ from django.contrib.auth.forms import UserCreationForm
 
 def index(request):
     print(request.user)
-    context = {}
+    friends = Friendship.objects.filter(to_user=request.user, approved=True)
+    context = {'friends': friends}
     return render(request, 'accounts/index.html', context)
 
 
-def friendship(request, receiver_id):
+def friend_request(request, receiver_id):
     print(receiver_id)
-    # friendship = Friendship()
-    pass
-
-
-def friend_request(request):
-    print(request.user.id)
+    receiver = User.objects.get(id=receiver_id)
+    if not Friendship.objects.filter(from_user=request.user, to_user=receiver).exists():
+        # doesnt exists
+        friendship = Friendship(from_user=request.user, to_user=receiver)
+        friendship.save()
+    else:
+        # exists
+        print("exists")
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+
+def friend_requests(request):
+    requests = Friendship.objects.filter(to_user=request.user, approved=False)
+    context = {'requests': requests}
+    return render(request, 'accounts/requests.html', context)
+
+
+def accept_friend(request, request_id):
+    if Friendship.objects.filter(id=request_id).exists():
+        request = Friendship.objects.filter(id=request_id).first()
+        request.approved = True
+        request.save()
+    return redirect('friend_requests')
+
+
+def get_friends(request):
+    user = request.user.id
+    print(user)
